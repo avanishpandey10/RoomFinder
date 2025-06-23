b71e8ae1f0f957ff9e740cbed9f41cbe5abeda4c
avanishpandey10 <avanishpandey8770@gmail.com>
2025-06-23 20:31:26 +0530

    Web app to find rooms for students who come from outer areas

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+// app.get("/demouser",async(req,res)=>{
+// //     let fakeUser = new User({
+// //         email:"student@gmail.com",
+// //         username:"delta-student"
+// //     });
+
+// //     let registeredUser = await User.register(fakeUser,"Helloworld");
+// //     res.send(registeredUser);
+// // })
